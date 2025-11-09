5e6010ec9008553fc48b0be1070077d0f949c4f5
WilliamLiangUofT <bill.c.liang@gmail.com>
2025-11-09 18:16:05 -0500

    add TransactionOutputData

--- src/main/java/use_case/add_transaction/AddTransactionOutputData.java
+++ src/main/java/use_case/add_transaction/AddTransactionOutputData.java
@@ -1,4 +1,33 @@
 package use_case.add_transaction;
 
 public class AddTransactionOutputData {
+    private String message;
+    private String accountNumber;
+    private double transactionAmount;
+    private double newBalance;
+
+    // Transaction of $amount added successfully to $accountNumber. New balance is $newBalance. Additional messages: $message
+
+    public AddTransactionOutputData(String message, String accountNumber, double transactionAmount, double newBalance) {
+        this.message = message;
+        this.accountNumber = accountNumber;
+        this.transactionAmount = transactionAmount;
+        this.newBalance = newBalance;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public double getTransactionAmount() {
+        return this.transactionAmount;
+    }
+
+    public double getNewBalance() {
+        return this.newBalance;
+    }
 }
