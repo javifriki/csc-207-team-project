d26736d48fb0363504d78adfdc6bdf14a63453d5
WilliamLiangUofT <bill.c.liang@gmail.com>
2025-11-18 22:03:19 -0500

    add ViewManager and ViewManagerViewModel

---
+++ src/main/java/interface_adaptor/ViewManagerViewModel.java
@@ -0,0 +1,9 @@
+package interface_adaptor;
+
+public class ViewManagerViewModel extends ViewModel<String> {
+
+    public ViewManagerViewModel() {
+        super("view manager");
+        this.setState("");
+    }
+}
