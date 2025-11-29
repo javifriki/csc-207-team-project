97647eb2311bc76ad184b797b1dac4fbb0ed7454
Amina <amina.enkhbayar@mail.utoronto.ca>
2025-11-29 17:57:18 -0500

    converter

---
+++ src/main/java/use_case/currency_converter/CurrencyConverterOutputBoundary.java
@@ -0,0 +1,7 @@
+package use_case.currency_converter;
+
+public interface CurrencyConverterOutputBoundary {
+    void present(CurrencyConverterOutputData outputData);
+    void presentError(String errorMessage);
+}
+
